ab0182a8de71a4dc8937210c58b6a0aefa72d8c4
linux_china <linux_china@hotmail.com>
2015-11-18 13:58:56 +0800

    wechat mp demo tests

---
+++ spring-boot-wechatmp-demo/src/test/java/org/mvnsearch/wechat/SpringBootWechatmpDemoApplicationTests.java
@@ -0,0 +1,23 @@
+package org.mvnsearch.wechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * wechat mp demo tests
+ *
+ * @author linux_china
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootWechatmpDemoApplication.class)
+@WebAppConfiguration
+public class SpringBootWechatmpDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
