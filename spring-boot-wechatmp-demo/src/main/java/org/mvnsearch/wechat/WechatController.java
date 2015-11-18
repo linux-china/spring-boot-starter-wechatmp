552facaad38bbe14c00642b8cb9033cf245c0012
linux_china <linux_china@hotmail.com>
2015-11-18 13:58:06 +0800

    use uuid as state

--- spring-boot-wechatmp-demo/src/main/java/org/mvnsearch/wechat/WechatController.java
+++ spring-boot-wechatmp-demo/src/main/java/org/mvnsearch/wechat/WechatController.java
@@ -3,11 +3,12 @@ package org.mvnsearch.wechat;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 /**
  * wechat controller
@@ -23,7 +24,8 @@ public class WechatController {
     @RequestMapping("/login")
     public String login() {
         String redirectUrl = "http://www.foobar.com/wechat/callback.action";
-        String snsapiLogin = wxMpService.oauth2buildAuthorizationUrl(redirectUrl, "snsapi_login", RandomStringUtils.randomAlphanumeric(8));
+        String state = UUID.randomUUID().toString();
+        String snsapiLogin = wxMpService.oauth2buildAuthorizationUrl(redirectUrl, "snsapi_login", state);
         return "redirect:" + snsapiLogin;
     }
 
