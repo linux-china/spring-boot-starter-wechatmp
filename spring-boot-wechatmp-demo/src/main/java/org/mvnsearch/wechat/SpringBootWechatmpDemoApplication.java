e4f146a8bf4bd8189e3bfd46501acb9e7fc9d8b5
linux_china <linux_china@hotmail.com>
2015-11-18 13:55:54 +0800

    spring boot wechatMP Demo application

---
+++ spring-boot-wechatmp-demo/src/main/java/org/mvnsearch/wechat/SpringBootWechatmpDemoApplication.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot wechatMP Demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class SpringBootWechatmpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWechatmpDemoApplication.class, args);
+    }
+}
