849e1a94561ae05288f3cf28ea19341d2377ca0d
linux_china <linux_china@hotmail.com>
2015-11-18 11:24:31 +0800

    wechat mp properties

---
+++ spring-boot-starter-wechatmp/src/main/java/org/mvnsearch/wechat/mp/WechatMpProperties.java
@@ -0,0 +1,62 @@
+package org.mvnsearch.wechat.mp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * wechat mp properties
+ *
+ * @author linux_china
+ */
+@ConfigurationProperties(
+        prefix = "spring.wechatmp"
+)
+public class WechatMpProperties {
+    /**
+     * 设置微信公众号的appid
+     */
+    private String appId;
+    /**
+     * 设置微信公众号的app secret
+     */
+    private String secret;
+    /**
+     * 设置微信公众号的token
+     */
+    private String token;
+    /**
+     * 设置微信公众号的EncodingAESKey
+     */
+    private String aesKey;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getAesKey() {
+        return aesKey;
+    }
+
+    public void setAesKey(String aesKey) {
+        this.aesKey = aesKey;
+    }
+}
